051d1805cfa8eee2d7babe906b36580453018037
Nugi29 <nugitha.c@gmail.com>
2025-09-23 13:00:00 +0530

    add routing and  implement Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Doctors from './pages/doctors'
+import Login from './pages/login'
+import Contact from './pages/contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:specialty' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+
+      </Routes>
 
     </div>
   )
 }
 
 export default App
+
