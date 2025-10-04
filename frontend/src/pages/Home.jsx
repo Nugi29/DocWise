f2dbddf271aa78f77c391e54cbc2ad3055daefa6
Nugi29 <nugitha.c@gmail.com>
2025-10-04 15:00:29 +0530

    add SpecialityMenu component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
+import SpecialaityMenu from '../components/specialityMenu.jsx'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <SpecialaityMenu/>
     </div>
   )
 }
