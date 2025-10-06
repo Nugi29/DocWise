9b72be38bae35554825450202519f1700c133474
Nugi29 <nugitha.c@gmail.com>
2025-10-06 10:46:13 +0530

    add Banner component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header.jsx'
 import SpecialaityMenu from '../components/specialityMenu.jsx'
 import TopDoctors from '../components/TopDoctors.jsx'
+import Banner from '../components/Banner.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header/>
       <SpecialaityMenu/>
       <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
