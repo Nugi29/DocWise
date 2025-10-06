d1d0494a687945cac930a608177ef0625711799f
Nugi29 <nugitha.c@gmail.com>
2025-10-06 10:17:09 +0530

    add TopDoctors component and integrate it into Home page; set up AppContext for managing doctors data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import SpecialaityMenu from '../components/specialityMenu.jsx'
+import TopDoctors from '../components/TopDoctors.jsx'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <SpecialaityMenu/>
+      <TopDoctors/>
     </div>
   )
 }
