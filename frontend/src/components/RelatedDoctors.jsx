4390c46effe264908fd2e0ac55d8fdeae0c2551b
Nugi29 <nugitha.c@gmail.com>
2025-10-08 19:24:40 +0530

    feat: add RelatedDoctors component to display doctors based on speciality in Appointment page

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,19 +1,29 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/AppContext.jsx';
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
+const RelatedDoctors = ({ speciality, docId }) => {
 
-const TopDoctors = () => {
-
+    const {doctors} = useContext(AppContext);
     const navigate = useNavigate();
-    const { doctors } = useContext(AppContext);
 
-    return (
+
+    const [relDoc,setRelDoc] = useState([]);
+
+    useEffect(() => {
+        if (doctors.length > 0 && speciality) {
+            const docatorsData = doctors.filter(doc => doc.speciality === speciality && doc._id !== docId);
+            setRelDoc(docatorsData);
+        }
+    }, [doctors, speciality, docId]);
+
+  return (
+    <div>
         <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
             <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-                {doctors.slice(0, 10).map((item, index) => (
+                {relDoc.slice(0, 5).map((item, index) => (
                     <div onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                         <img className='bg-blue-50' src={item.image} alt={item.name} />
                         <div className='p-4'>
@@ -28,7 +38,8 @@ const TopDoctors = () => {
             </div>
             <button onClick={() => {navigate('/doctors'); scrollTo(0, 0);}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 cursor-pointer'>more</button>
         </div>
-    )
+    </div>
+  )
 }
 
-export default TopDoctors
+export default RelatedDoctors
