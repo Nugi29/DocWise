a3f5126da5eaa64cb5d86b5427ba049c352b2688
Nugi29 <nugitha.c@gmail.com>
2025-10-19 16:34:31 +0530

    feat: implement sidebar navigation and add admin pages for dashboard, appointments, and doctors

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,12 @@ import Login from './pages/Login'
 import { AdminContext } from './context/AdminContext.jsx';
 import { ToastContainer, toast } from 'react-toastify';
 import Navbar from './components/Navbar.jsx';
-
+import Sidebar from './components/Sidebar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard.jsx';
+import AllAppointments from './pages/Admin/AllAppointments.jsx';
+import AddDoctor from './pages/Admin/AddDoctor.jsx';
+import DoctorsList from './pages/Admin/DoctorsList.jsx';
 
 
 const App = () => {
@@ -13,6 +18,16 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctors-list' element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <div>
