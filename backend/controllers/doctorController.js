4800f6abf5da12fc006e1e85f68c41d7d1253946
Nugi29 <nugitha.c@gmail.com>
2025-10-20 13:53:02 +0530

    feat: add doctor listing functionality and update routing for doctor API

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,7 +1,5 @@
 import doctorModel from "../models/doctorModel.js";
 
-
-
 const changeAvailability = async (req, res) => {
     try {
         const { docId } = req.body;
@@ -13,4 +11,15 @@ const changeAvailability = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 };
-export { changeAvailability };
+
+const doctorList = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+        res.json({ success: true, doctors });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+export { changeAvailability, doctorList };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Doctors from './pages/doctors'
@@ -9,10 +9,13 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
